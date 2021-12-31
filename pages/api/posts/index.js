56d1bf64d87eeb5220e5749bc32fa59e2ef70d6c
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-31 19:38:21 +0700

    update middleware posts require token

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,8 +1,33 @@
 import database from "../../../library/connection"
 
-const handler = async(request, response) => {
-    if (request.method !== "GET") response.status(405).end()
+import jwt from "jsonwebtoken"
 
+require("dotenv/config")
+const SECRET_KEY = process.env.SECRET_KEY
+
+const handler = async(request, response) => {
+    const { authorization } = request.headers
+    if (!authorization) response.status(401).end()
+
+    const separatedAuth = authorization.split(' ')
+    console.log(separatedAuth);
+
+    // const [typeAuthentication, tokenAuthentication] = [separatedAuth[0], separatedAuth[1]]
+
+    const typeAuthentication = authorization.split(" ")[0]
+    const tokenAuthentication = authorization.split(" ")[1]
+
+    console.log(`Type Auth : ${typeAuthentication}`);
+    console.log(`Token Auth : ${tokenAuthentication}`);
+
+    if (typeAuthentication !== "Bearer") response.status(401).end()
+
+    const verifyTokens = jwt.verify(tokenAuthentication, SECRET_KEY)
+
+    const { user, email } = verifyTokens
+    console.log(user, email);
+
+    if (request.method !== "GET") response.status(405).end()
     const posts = await database("posts")
 
     response.status(200)
