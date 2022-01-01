ac01daa94c4375ad7c1b70aa582c04c7678bb2c2
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-01 11:45:50 +0700

    update post create

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,33 +1,12 @@
 import database from "../../../library/connection"
-
-import jwt from "jsonwebtoken"
-
-require("dotenv/config")
-const SECRET_KEY = process.env.SECRET_KEY
+import authorizeToken from "../../../middlewares/AuthToken"
 
 const handler = async(request, response) => {
-    const { authorization } = request.headers
-    if (!authorization) response.status(401).end()
-
-    const separatedAuth = authorization.split(' ')
-    console.log(separatedAuth);
-
-    // const [typeAuthentication, tokenAuthentication] = [separatedAuth[0], separatedAuth[1]]
-
-    const typeAuthentication = authorization.split(" ")[0]
-    const tokenAuthentication = authorization.split(" ")[1]
-
-    console.log(`Type Auth : ${typeAuthentication}`);
-    console.log(`Token Auth : ${tokenAuthentication}`);
-
-    if (typeAuthentication !== "Bearer") response.status(401).end()
-
-    const verifyTokens = jwt.verify(tokenAuthentication, SECRET_KEY)
-
-    const { user, email } = verifyTokens
-    console.log(user, email);
-
     if (request.method !== "GET") response.status(405).end()
+
+    const auth = await authorizeToken(request, response)
+    console.log(auth);
+
     const posts = await database("posts")
 
     response.status(200)
