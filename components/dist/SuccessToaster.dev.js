13700dcc06c336152196624037bdf44966532343
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-02 18:11:56 +0700

    update toaster components

--- components/dist/SuccessToaster.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var SuccessToaster = function SuccessToaster() {
-  return di;
-};
