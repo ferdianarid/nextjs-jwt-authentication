2960128337c8fb5d9bc5b89c55eadf613fa19982
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-02 18:12:30 +0700

    add posts pages

---
+++ pages/posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Posts = () => {
+       return (
+              <div>
+                     <h1 className='text-2xl font-bold'>Posts List</h1>
+                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, architecto!</p>
+              </div>
+       )
+}
+
+export default Posts
